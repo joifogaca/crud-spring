f6bee05e8b0bab03d9aabdbf98f6e27db221bf56
Joice Fogaça <joi.fogaca@gmail.com>
2024-03-24 14:12:00 -0300

    first commit

---
+++ src/test/java/com/joi/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joi.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
