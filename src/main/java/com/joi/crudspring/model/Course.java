1522387294ea7ab2d1b29320b74584b1152d10f6
Joice Fogaça <joi.fogaca@gmail.com>
2024-04-13 21:09:05 -0300

    feat: :sparkles: Add soft delete

--- src/main/java/com/joi/crudspring/model/Course.java
+++ src/main/java/com/joi/crudspring/model/Course.java
@@ -1,5 +1,9 @@
 package com.joi.crudspring.model;
 
+
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.SQLDelete;
+
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,12 +13,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data // Lombok gerar gette e setter
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@SQLRestriction("status <> 'Inativo'")
+
 public class Course {
 
     @Id
@@ -22,14 +29,21 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    @NotBlank
+    @NotNull
     @jakarta.validation.constraints.NotNull
     @Length(min = 3, max = 100)
     @Column(length = 100, nullable = false)
     private String name;
 
+    @NotNull
+    @Length(max = 10)
     @Column(length = 10, nullable = false)
-    @Length(max = 100)
     @Pattern(regexp = "Back-end|Front-end")
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Column(length = 10, nullable = false)
+    @Pattern(regexp = "Ativo|Inativo")
+    private String status = "Ativo";
 }
