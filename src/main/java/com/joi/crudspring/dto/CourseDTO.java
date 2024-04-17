2cf76cbcef8c442e82b0c33a6c1668348e866161
Joice Fogaça <joi.fogaca@gmail.com>
2024-04-17 11:34:21 -0300

    refactor: :recycle: Criação record DTO

---
+++ src/main/java/com/joi/crudspring/dto/CourseDTO.java
@@ -0,0 +1,17 @@
+package com.joi.crudspring.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CourseDTO(
+    @JsonProperty("_id") Long id,
+    @NotNull @NotBlank @Length(min = 3, max = 100) String name,
+    @NotNull @Length(max = 10) @Column(length = 10, nullable = false) @Pattern(regexp = "Back-end|Front-end") String category) 
+    {
+}
--- src/main/java/com/joi/crudspring/model/Course.java
+++ src/main/java/com/joi/crudspring/model/Course.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -30,7 +31,7 @@ public class Course {
     private Long id;
 
     @NotNull
-    @jakarta.validation.constraints.NotNull
+    @NotBlank
     @Length(min = 3, max = 100)
     @Column(length = 100, nullable = false)
     private String name;
