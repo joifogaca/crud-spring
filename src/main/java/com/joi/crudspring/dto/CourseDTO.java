95e730b678f1b8035dacc5467f88bdc0ea753a76
Joice Fogaça <joi.fogaca@gmail.com>
2024-04-22 11:28:51 -0300

    feat:

--- src/main/java/com/joi/crudspring/dto/CourseDTO.java
+++ src/main/java/com/joi/crudspring/dto/CourseDTO.java
@@ -1,9 +1,12 @@
 package com.joi.crudspring.dto;
 
+import java.util.List;
+
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.joi.crudspring.enums.Category;
+import com.joi.crudspring.model.Lesson;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
@@ -12,5 +15,6 @@ import jakarta.validation.constraints.NotNull;
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotNull @NotBlank @Length(min = 3, max = 100) String name,
-        @NotNull @Column(length = 10, nullable = false) String category) {
+        @NotNull @Column(length = 10, nullable = false) String category,
+        List<Lesson> lessons) {
 }
