a3180982003529f8d09b3d20472ddd3037fbd74f
Joice Fogaça <joi.fogaca@gmail.com>
2024-05-04 11:37:31 -0300

    perf: :safety_vest: Ajuste de Validação de Categoria

--- src/main/java/com/joi/crudspring/dto/CourseDTO.java
+++ src/main/java/com/joi/crudspring/dto/CourseDTO.java
@@ -5,16 +5,18 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.joi.crudspring.enums.Category;
+import com.joi.crudspring.enums.validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+
 
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotNull @NotBlank @Valid @Length(min = 3, max = 100) String name,
-        @NotNull @NotBlank @Valid @Pattern(regexp = "Back-end|Front-end")  String category,
+        @NotNull @NotBlank @Valid @ValueOfEnum(enumClass = Category.class) String category,
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
